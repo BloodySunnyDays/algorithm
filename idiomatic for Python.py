fdd5ee36d2f07debf09d2f82365b44a90a14d3e4
BloodySunnyDays <596075962@qq.com>
2017-07-01 02:35:50 -0700

    2017-07-01

--- idiomatic for Python.py
+++ idiomatic for Python.py
@@ -44,14 +44,14 @@ print (','.join(names))
 
 # 5.open/close file
 # ordinary
-f = open('data.txt')
+f = open('data')
 try:
     data = f.read()
 finally:
     f.close()
 
 # pythonic
-with open('data.txt') as f:
+with open('data') as f:
     data2 = f.read()
 
 
@@ -63,7 +63,7 @@ for i in range(10):
     result.append(s)
 
 # pythonic
-[i*2 for i in xrange(10)]
+print [i*2 for i in xrange(10)]
 
 # 7.decorator
 import urllib
@@ -86,7 +86,7 @@ def cache(func):
             page = func(url)
             saved[url] = page
             return page
-    return wrapper()
+    return wrapper
 
 @cache
 def web_lookup(url):
@@ -146,6 +146,120 @@ if age2 > 18 and age<60:
 # pythonic
 if 18 < age < 60:
     print "young man"
+
+
+# 2.if/else 三目运算
+# ordinary
+if gender == 'male':
+    text = '男'
+else:
+    text = '女'
+
+# pythonic
+text2 = '男' if gender =='male' else '女'
+
+# Pythonic has a word：
+# “There should be one-- and preferably only one --obvious way to do it. ”。
+
+# 3.Truth value judgment
+# ordinary
+attr = ''
+values = []
+def do_something():
+    pass
+
+if attr == True:
+    do_something()
+
+if len(values)!=0:
+    do_something()
+
+
+# pythonic
+if attr:
+    do_something()
+if values:
+    do_something()
+
+
+# 4.for/else :
+# ordinary
+mylist =[]
+theflag=1
+def process(n):
+    print n
+flagfound = False
+for i in mylist:
+    if i == theflag:
+        flagfound = True
+        break
+    process(1)
+
+if not flagfound:
+    raise ValueError('List argument missing terminal flag.')
+
+# pythonic
+for i in mylist:
+    if i==theflag:
+        break
+    process(i)
+else:
+    raise ValueError('List argument missing terminal flag')
+
+
+# 5.Character String format
+# ordinary
+s1 = 'foofish.net'
+s2 = 'vttalk'
+s3 = 'welcom to %s and following %s' % (s1,s2)
+
+# pythonic
+s4 = 'welcom to {blog} and following {wechat}'.format(blog='foofish.net',wechat='vttalk')
+
+# 6 list slice
+# ordinay
+items = range(10)
+
+# odd
+odd_items = []
+for i in items:
+    if i%2 != 0:
+        odd_items.append(i)
+
+#Copy
+copy_items = []
+for i in items:
+    copy_items.append(i)
+
+# pythonic
+# A range of first to fourth elements
+sub_items = items[1:4]
+# odd
+odd_items = items[1::2]
+# copy
+copy_items = items[::]
+# or
+copy_items = items[:]
+# 列表元素的下标不仅可以用正数表示，还是用负数表示，
+# 最后一个元素的位置是 -1，从右往左，依次递减。
+"""
+--------------------------
+ | P | y | t | h | o | n |
+--------------------------
+   0   1   2   3   4   5 
+  -6  -5  -4  -3  -2  -1
+--------------------------
+"""
+
+
+
+
+
+
+
+
+
+
 
 
 
